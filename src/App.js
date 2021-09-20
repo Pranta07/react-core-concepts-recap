268ce0f7ba43b4b530da89a9b94fdcc9cd7336b1
Pranta07 <palpranta@gmail.com>
2021-09-20 23:26:20 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <MyComponent name="React" salary="20k"></MyComponent>
+            <MyComponent name="Google" salary="30k"></MyComponent>
+            <MyComponent name="Microsoft" salary="35k"></MyComponent>
+            <MyComponent name="Facebook" salary="25k"></MyComponent>
+            <LoadUsers></LoadUsers>
+        </div>
+    );
+}
+
+function User(props) {
+    return (
+        <div className="btn">
+            <h3>Id: {props.id}</h3>
+            <p>Name: {props.name}</p>
+            <p>Email: {props.email}</p>
+        </div>
+    );
+}
+
+function LoadUsers() {
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/users")
+            .then((res) => res.json())
+            .then((users) => setUsers(users));
+    }, []);
+    return (
+        <div>
+            <h1>Loaded Users: {users.length}</h1>
+
+            {users.map((user) => (
+                <User id={user.id} name={user.name} email={user.email}></User>
+            ))}
+        </div>
+    );
+}
+
+function MyComponent(props) {
+    const [bonus, setBonus] = useState(0);
+    const myStyle = {
+        backgroundColor: "lightsalmon",
+        border: "1px solid blue",
+        borderRadius: "10px",
+        margin: "10px",
+    };
+    const addBonus = () => {
+        setBonus(bonus + 500);
+    };
+    return (
+        <div style={myStyle}>
+            <h2>Hello {props.name} Mama!</h2>
+            <p>I can Create React Component!</p>
+            <p>Salary: {props.salary}</p>
+            <p>Bonus: {bonus}</p>
+            <button className="btn" onClick={addBonus}>
+                Add Bonus
+            </button>
+        </div>
+    );
 }
 
 export default App;
